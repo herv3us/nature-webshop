ab1534adef7dad62fe7d16aa983ea88989ffc431
Sofia Hervéus <sofia@herveus.se>
2022-02-11 16:33:12 +0100

    work on more tests for searchform

--- frontend/src/components/tests/SearchForm.test.tsx
+++ frontend/src/components/tests/SearchForm.test.tsx
@@ -1,7 +1,24 @@
 import SearchForm from '../SearchForm';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { search } from '../../utils/search';
+import { getAllProducts } from '../../services/productService';
+import { products } from '../../dummyData/products';
 
 const setSearchStringMock = jest.fn();
+const searchStringMock = 'backpack';
+
+jest.mock('../../utils/search', () => {
+  return {
+    search: jest.fn() as jest.Mock,
+  };
+});
+
+jest.mock('../../services/productService', () => {
+  return {
+    getAllProducts: jest.fn() as jest.Mock,
+  };
+});
 
 describe('Tests for SearchForm', () => {
   it('render without crashing', () => {
@@ -23,5 +40,22 @@ describe('Tests for SearchForm', () => {
 
     const inputElem = screen.getByPlaceholderText(/sök/i);
     expect(inputElem).toBeInTheDocument();
+  });
+
+  it('search for backpacks when typing backpack in inputfiled', () => {
+    (search as jest.Mock<any>).mockImplementation(() => 'backpack');
+    (getAllProducts as jest.Mock<any>).mockImplementation(() => products);
+
+    render(
+      <SearchForm
+        searchString={searchStringMock}
+        setSearchString={setSearchStringMock}
+      />
+    );
+
+    const inputElem = screen.getByPlaceholderText(/sök/i);
+    userEvent.type(inputElem, 'backpack');
+
+    expect(setSearchStringMock).toHaveBeenCalledTimes(8);
   });
 });
